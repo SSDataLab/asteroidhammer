f5d70f97d1e1c5d8b5dc5e7aa27224031418f512
Christina Hedges <christina.l.hedges@nasa.gov>
2021-02-11 15:18:46 -0800

    :hammer: able to save bkg and reconstruct
    able to save bkg pickle file and reconstruct design matrix for pixel

--- src/asteroidhammer/background.py
+++ src/asteroidhammer/background.py
@@ -10,6 +10,8 @@ import pickle
 
 import lightkurve as lk
 
+from fbpca import pca
+
 from astropy.io import fits
 from astropy.wcs import WCS
 from astropy.time import Time
@@ -29,9 +31,11 @@ def _count_unq(locs):
     return k[j]
 
 
-_keys = ['nfiles', 'nchunks', 'npixels', 'magnitude_limit', 'testframe', 'camera', 'ccd', 'sector',
+_keys = ['fnames', 'nfiles', 'nchunks', 'npixels', 'magnitude_limit', 'testframe', 'camera', 'ccd', 'sector',
         'wcs', 'locs', 'dra', 'ddec', 'phot', 'mag', 'star_aper',
         'mask', 'bright_mask', 'bkg_aper', 'avg', 'w', 'dra_bin', 'ddec_bin', 'med', 'dmed']
+_keys_lite = ['fnames', 'nfiles', 'nchunks', 'npixels', 'magnitude_limit', 'testframe', 'camera', 'ccd', 'sector', 'w', 'dra_bin', 'ddec_bin', 'med', 'dmed']
+
 
 ## Array for circular apertures as a function of g magnitude...
 _christina_guess = np.asarray([[0, 10], [1, 10], [2, 10], [3, 9], [4, 7], [5, 5], [6, 4], [8, 4], [10, 4], [11, 3], [12, 3], [13, 3], [14, 2], [15, 2], [16, 2], [17, 1]])
@@ -39,7 +43,7 @@ _christina_guess = np.asarray([[0, 10], [1, 10], [2, 10], [3, 9], [4, 7], [5, 5]
 class BackgroundCorrector(object):
 
 
-    def __init__(self, fnames=None, nchunks=16, testframe=None, magnitude_limit=16, nbins=(30, 30)):
+    def __init__(self, fnames=None, nchunks=16, testframe=None, magnitude_limit=16):
         """Parameters
 
         fnames : list of str
@@ -84,72 +88,65 @@ class BackgroundCorrector(object):
             self.mask = (self.mag < 13) & (count == 1) & (np.hypot(self.dra, self.ddec) <= 2.5 * np.median(np.hypot(np.diff(ra[0]), np.diff(dec[:, 0]))))
             self.bright_mask = np.zeros((2078, 2136), bool)
             self.bright_mask[self.locs[0][self.mask], self.locs[1][self.mask] + 45] = True
-            # small for test
-            self.bright_mask = self.bright_mask[:self.npixels*1, 45:self.npixels*16+45].ravel()
+            self.bright_mask = self.bright_mask[:2048, 45:2048+45].ravel()
 
 
-            f = fitsio.read(fnames[self.testframe], ext=1)
             bkg_aper = ~self.star_aper
-            bkg_aper[f < 10] = False
-
-            # For testing
-            bkg_aper[self.npixels:] = False
-
-            bkg_aper &= f < np.nanpercentile(f[bkg_aper], 99.5)
-            bkg_aper &= f > np.nanpercentile(f[bkg_aper], 0.05)
             self.bkg_aper = bkg_aper
+            self.Xf = self._get_X(c=np.arange(2048), r=np.arange(2048))
 
-            # Row short for test!
-            self.Xf = self._get_X(c=np.arange(2048), r=np.arange(128))
+    def find_bkg_solution(self, nbins=(30, 30)):
+        # Clip out outliers present in the test frame
+        f = fitsio.read(self.fnames[self.testframe], ext=1)
+        f = f[:2048, 45:2048+45].ravel()
+        fe = fitsio.read(self.fnames[self.testframe], ext=2)[:2048, 45:2048+45].ravel()
+        mask = self.bkg_aper.copy()[:2048, 45:2048+45].ravel()
+        mask[f < 10] = False
+        mask &= f < np.nanpercentile(f[mask], 99.9)
+        mask &= f > np.nanpercentile(f[mask], 0.01)
 
-            # if os.path.isdir('.database'):
-            #     [os.remove(f) for f in glob('.database/*')]
-            #     os.removedirs('.database')
-            # os.mkdir('.database')
+        # Prior mu is all zero...
+        prior_sigma = np.ones(self.Xf.shape[1]) * 3000
 
-            # Clip out outliers present in the test frame
-            f = f[:self.npixels*1, 45:self.npixels*16+45].ravel()
-            fe = fitsio.read(self.fnames[self.testframe], ext=2)[:self.npixels*1, 45:self.npixels*16+45].ravel()
-            mask = bkg_aper[:self.npixels, 45:45+self.npixels*16].ravel()
-            sigma_w_inv = self.Xf.T[:, mask].dot(self.Xf[mask]).toarray()
-            B = self.Xf.T[:, mask].dot(f[mask])
-            w = np.linalg.solve(sigma_w_inv, B)
-            mod = self.Xf.dot(w)
-            chi = (f - mod)**2/fe**2
+        sigma_w_inv = self.Xf.T[:, mask].dot(self.Xf[mask]).toarray() + np.diag(1/prior_sigma**2)
+        B = self.Xf.T[:, mask].dot(f[mask])
+        w = np.linalg.solve(sigma_w_inv, B)
+        mod = self.Xf.dot(w)
+        chi = (f - mod)**2/fe**2
 
-            mask &= (chi < np.percentile(chi, 95))
+        # Clip out heinous outliers
+        mask &= (chi < np.percentile(chi, 99.9))
 
-            sigma_w_inv = self.Xf.T[:, mask].dot(self.Xf[mask]).toarray()
-            XfTk = self.Xf.T[:, mask]
+        sigma_w_inv = self.Xf.T[:, mask].dot(self.Xf[mask]).toarray() + np.diag(1/prior_sigma**2)
+        XfTk = self.Xf.T[:, mask]
+        B = XfTk.dot(f.ravel()[mask])
+        self.avg = (f - self.Xf.dot(np.linalg.solve(sigma_w_inv, B)))[self.bright_mask]
+
+
+        self.w = np.zeros((self.nfiles, self.Xf.shape[1]))
+        Xf_bright = self.Xf[self.bright_mask]
+
+        self.dra_bin, self.ddec_bin, bin_masks = self._get_bin_basis(nbins=nbins)
+        self.med = np.asarray([np.nanmedian(self.avg[m1]) for m1 in bin_masks]).reshape(nbins)
+
+
+#        self.avg = np.zeros(self.bright_mask.sum())
+        self.dmed = np.ones((self.nfiles, *nbins))
+        for idx, fname in enumerate(tqdm(self.fnames, desc='Calculating Background Correction')):
+            f = fitsio.read(fname, ext=1)[:2048, 45:2048+45]
+            #fe = fitsio.read(fname, ext=2)[:self.npixels*1, 45:self.npixels*16+45]
+
             B = XfTk.dot(f.ravel()[mask])
-            self.avg = (f - self.Xf.dot(np.linalg.solve(sigma_w_inv, B)))[self.bright_mask]
+            self.w[idx] = np.linalg.solve(sigma_w_inv, B)
 
+            # Need to extract stars here
+            f_bright = f.ravel()[self.bright_mask] - Xf_bright.dot(self.w[idx])
+            self.dmed[idx] = np.asarray([np.nanmedian(f_bright[m1]/self.avg[m1]) if m1.sum() != 0 else 1 for m1 in bin_masks]).reshape(nbins)
 
-            self.w = np.zeros((self.nfiles, self.Xf.shape[1]))
-            # Small for test
-            Xf_bright = self.Xf[self.bright_mask]
-
-            self.dra_bin, self.ddec_bin, bin_masks = self._get_bin_basis(nbins=nbins)
-            self.med = np.asarray([np.nanmedian(self.avg[m1]) for m1 in bin_masks]).reshape(nbins)
-
-
-    #        self.avg = np.zeros(self.bright_mask.sum())
-            self.dmed = np.ones((self.nfiles, *nbins))
-            for idx, fname in enumerate(tqdm(self.fnames, desc='Calculating Background Correction')):
-                f = fitsio.read(fname, ext=1)[:self.npixels*1, 45:self.npixels*16+45]
-                #fe = fitsio.read(fname, ext=2)[:self.npixels*1, 45:self.npixels*16+45]
-
-                B = XfTk.dot(f.ravel()[mask])
-                self.w[idx] = np.linalg.solve(sigma_w_inv, B)
-
-                # Need to extract stars here
-                f_bright = f.ravel()[self.bright_mask] - Xf_bright.dot(self.w[idx])
-                self.dmed[idx] = np.asarray([np.nanmedian(f_bright[m1]/self.avg[m1]) if m1.sum() != 0 else 1 for m1 in bin_masks]).reshape(nbins)
-
-                #table = pa.table({'flux': f_bright})
-                #pq.write_table(table, f'.database/{idx:04}.parquet')
-    #            self.avg += f_bright
-    #        self.avg /= self.nfiles
+            #table = pa.table({'flux': f_bright})
+            #pq.write_table(table, f'.database/{idx:04}.parquet')
+#            self.avg += f_bright
+#        self.avg /= self.nfiles
 
     def _get_bin_basis(self, nbins=(20, 20)):
         s = np.lexsort((self.locs[1][self.mask], self.locs[0][self.mask]))
@@ -166,10 +163,13 @@ class BackgroundCorrector(object):
                 m.append((dra > x1) & (dra < x[idx + 1]) & (ddec > y1) & (ddec < y[jdx + 1]))
         return x[:-1] - np.median(np.diff(x))/2, y[:-1] - np.median(np.diff(y))/2, m
 
-    def save(self, out=None):
+    def save(self, out=None, lite=False):
         if out is None:
             out = f'sec{self.sector:04}_camera{self.camera}_ccd{self.ccd}.tessbkg'
-        d = {key:getattr(self, key) for key in _keys}
+        if lite:
+            d = {key:getattr(self, key) for key in _keys_lite if hasattr(self, key)}
+        else:
+            d = {key:getattr(self, key) for key in _keys if hasattr(self, key)}
         # this should be parquet
         pickle.dump(d, open(out, 'wb'))
 
@@ -179,15 +179,14 @@ class BackgroundCorrector(object):
         b = BackgroundCorrector()
         # this should be parquet
         d = pickle.load(open(filename, 'rb'))
-        _ = [setattr(b, key, d[key]) for key in _keys]
-        b.Xf = b._get_X(c=np.arange(2048), r=np.arange(128))
+        _ = [setattr(b, key, d[key]) for key in _keys if key in d.keys()]
+        b.Xf = b._get_X(c=np.arange(2048), r=np.arange(2048))
         return b
 
     def get_full_bkg_frame(self, frames=0):
-        # Smaller for test!!!
         if not hasattr(frames, '__iter__'):
             frames = [np.copy(frames)]
-        model =  np.asarray([self.Xf.dot(self.w[frame]).reshape((128, 2048)) for frame in frames])
+        model =  np.asarray([self.Xf.dot(self.w[frame]).reshape((2048, 2048)) for frame in frames])
         if len(frames) == 1:
             return model[0]
         return model
@@ -195,11 +194,11 @@ class BackgroundCorrector(object):
     def get_bkg_cutout(self, loc=[[45, 128], [52, 90]], frames=0):
         # Shortened for test!
         c = np.arange(2048, dtype=int)
-        r = np.arange(128, dtype=int)
+        r = np.arange(2048, dtype=int)
 
-        ck = sparse.csr_matrix(((c > loc[1][0]) & (c <= loc[1][1])).astype(float)).T
-        rk = sparse.csr_matrix(((r > loc[0][0]) & (r <= loc[0][1])).astype(float)).T
-        ck = (sparse.vstack([ck for idx in range(128)], format='csr'))
+        ck = sparse.csr_matrix(((c >= loc[1][0]) & (c < loc[1][1])).astype(float)).T
+        rk = sparse.csr_matrix(((r >= loc[0][0]) & (r < loc[0][1])).astype(float)).T
+        ck = (sparse.vstack([ck for idx in range(2048)], format='csr'))
         rk = sparse.hstack([rk for idx in range(2048)]).reshape((ck.shape[0], rk.shape[1])).tocsr()
         Xf = self.Xf.multiply(ck).multiply(rk)
         # Only bother with pixels that actually have values
@@ -214,8 +213,8 @@ class BackgroundCorrector(object):
     def _get_X(self, c, r):
         """Make the X array we'll use for linear algebra """
         Xcf = lk.designmatrix.create_spline_matrix(c, knots=list(np.linspace(0, 2048, 82)[1:-1]), degree=3).append_constant().to_sparse().X.tocsr()
-        Xcf = sparse.vstack([Xcf for idx in range(128)])
-        Xrf = lk.designmatrix.create_spline_matrix(r, knots=list(np.linspace(0, 128, 6)[1:-1]), degree=3).append_constant().to_sparse().X.tocsr()
+        Xcf = sparse.vstack([Xcf for idx in range(2048)])
+        Xrf = lk.designmatrix.create_spline_matrix(r, knots=list(np.linspace(0, 2048, 82)[1:-1]), degree=3).append_constant().to_sparse().X.tocsr()
         Xrf = sparse.hstack([Xrf for idx in range(2048)]).reshape((Xcf.shape[0], Xrf.shape[1])).tocsr()
         Xf = sparse.hstack([Xrf.multiply(X.T) for X in Xcf.T]).tocsr()
         return Xf
@@ -272,6 +271,4 @@ class BackgroundCorrector(object):
                 dras.append(dra)
                 ddecs.append(ddec)
                 phots.append(phot)
-            # For testing!
-            break
         return np.hstack(locs), np.hstack(dras), np.hstack(ddecs), np.hstack(phots)
